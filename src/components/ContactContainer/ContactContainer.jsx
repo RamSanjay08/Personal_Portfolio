ce7fc8a46af8732a3d1fae298e3f35804cf5c88c
Ram Sanjay <ramsanjaydev08@gmail.com>
2023-11-16 21:07:22 +0530

    form data added

--- src/components/ContactContainer/ContactContainer.jsx
+++ src/components/ContactContainer/ContactContainer.jsx
@@ -1,10 +1,25 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import { Element } from "react-scroll";
 import ContactStyles from "./ContactContainer.module.css";
 import emailjs from "@emailjs/browser";
 import ContactFooter from "./ContactFooter";
+import { ToastContainer, toast } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css'
 
 const ContactContainer = () => {
+
+  let [getInput, setGetInput] = useState({user_name:"",user_email:"",message:""})
+
+  const handleInput = ({target:{name,value}}) => {
+    setGetInput({...getInput, [name]:value})
+  }
+
+  const Notify = () => {
+    toast.success("Message Sended Sucessfully", {
+      position: toast.POSITION.TOP_CENTER
+    })
+  } 
+
   const form = useRef();
 
   const sendEmail = (e) => {
@@ -19,18 +34,16 @@ const ContactContainer = () => {
       )
       .then(
         (result) => {
+          setGetInput({user_name:"",user_email:"",message:""})
           console.log(result.text);
+          Notify()
           console.log("Successfull");
         },
         (error) => {
           console.log(error.text);
         }
       )
-      .finally(() => {
-        form.current.reset();
-      });
-      
-  };
+  };  
 
   return (
     <Element name="contact" className={ContactStyles.ContactContainer}>
@@ -38,12 +51,13 @@ const ContactContainer = () => {
       <form ref={form} onSubmit={sendEmail}>
       <h3>GET IN TOUCH</h3>
         <label>Name</label>
-        <input type="text" name="user_name" maxLength="25" required/>
+        <input type="text" name="user_name" maxLength="25" required onChange={handleInput} value={getInput.user_name}/>
         <label>Email</label>
-        <input type="email" name="user_email" maxLength="25" required/>
+        <input type="email" name="user_email" maxLength="25" required onChange={handleInput} value={getInput.user_email}/>
         <label>Message</label>
-        <textarea name="message" required/> 
+        <textarea name="message" required onChange={handleInput}  value={getInput.message}/> 
         <input type="submit" value="Send" className={ContactStyles.sub_btn}/>
+        <ToastContainer theme="colored"/>
       </form>
       <ContactFooter/>
     </Element>
