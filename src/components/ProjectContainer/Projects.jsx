c1dd61e3358146327ad51ec70f08074d66894a8f
Ram Sanjay <ramsanjaydev08@gmail.com>
2023-11-20 00:25:34 +0530

    ui done

--- src/components/ProjectContainer/Projects.jsx
+++ src/components/ProjectContainer/Projects.jsx
@@ -1,6 +1,7 @@
   import PortfolioGenerator from '../Assests/Portfolio_Generator.png' 
   import MovieRatingApp from '../Assests/Movie_Rating_App.png'
   import ImageSearchApp from '../Assests/Image_Search_App.png'
+  import FoodApp from '../Assests/Food_App.png'
   
   const Projects = [
     {
@@ -8,21 +9,30 @@
       ImageSrc: PortfolioGenerator,
       title: 'Portfolio Generator',
       description: 'Here is an auto-generator portfolio web application that provides a hassle-free solution for crafting a stunning portfolio',
-      skills: ["React","Bootstrap","JSX"],
+      skills: ["React","Bootstrap"],
       sourceLink: 'https://github.com/RamSanjay08/Portfolio-Generator',
       liveLink: 'https://main--chimerical-duckanoo-20a2ca.netlify.app/'
     },
     {
       id: 2,
-      ImageSrc: MovieRatingApp,
-      title: 'IMDb Movie Rating App',
-      description: 'In the MovieRatingApp, you can view movie ratings from high to low on IMDb, helping you decide which movies to watch',
-      skills: ["React","Module CSS","APIs"],
-      sourceLink: 'https://github.com/RamSanjay08/React-Movie-App',
-      liveLink: 'https://react-movie-pkb4emkez-ram-sanjays-projects.vercel.app/'
+      ImageSrc: FoodApp,
+      title: 'Food Web App',
+      description: 'This web app provides a diverse range of images that you can explore through search, and it offers the option to download them easily',
+      skills: ["React","Tailwind CSS"],
+      sourceLink: 'https://github.com/RamSanjay08/Food-App-React',
+      liveLink: 'https://food-app-react-gnxqe1a9a-ram-sanjays-projects.vercel.app/'
     },
     {
       id: 3,
+      ImageSrc: MovieRatingApp,
+      title: 'IMDb Movie Rating App',
+      description: 'In the MovieRatingApp, you can view movie ratings from high to low on IMDb, helping you decide which movies to watch',
+      skills: ["React","APIs","CSS3"],
+      sourceLink: 'https://github.com/RamSanjay08/React-Movie-App',
+      liveLink: 'https://react-movie-app-gray-kappa.vercel.app/'
+    },
+    {
+      id: 4,
       ImageSrc: ImageSearchApp,
       title: 'Image Search App',
       description: 'This web app provides a diverse range of images that you can explore through search, and it offers the option to download them easily',
