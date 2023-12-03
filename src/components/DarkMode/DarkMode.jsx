3f23bac21677f74665b3760b8b2f6e597a5c2bc6
Ram Sanjay <ramsanjaydev08@gmail.com>
2023-12-03 13:15:19 +0530

    added

--- src/components/DarkMode/DarkMode.jsx
+++ src/components/DarkMode/DarkMode.jsx
@@ -32,8 +32,8 @@ const DarkMode = () => {
     <div className="dark_mode">
       <input className="dark_mode_input" type="checkbox" id="darkmode-toggle" onChange={toogleTheme} defaultChecked={selectedTheme === "dark"}/>
       <label className="dark_mode_label" htmlFor="darkmode-toggle">
-        <Sun />
         <Moon />
+        <Sun />
       </label>
     </div>
   );
