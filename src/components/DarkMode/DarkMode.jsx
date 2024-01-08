1448b7330f267b01ccb49a2625729d971a74798d
Ram Sanjay <ramsanjaydev08@gmail.com>
2024-01-08 10:47:00 +0530

    added

--- src/components/DarkMode/DarkMode.jsx
+++ src/components/DarkMode/DarkMode.jsx
@@ -8,7 +8,6 @@ const DarkMode = () => {
     document.querySelector("body").setAttribute("data-theme", "dark");
     localStorage.setItem("selectedTheme", "dark")
   };
-
   const setLightMode = () => {
     document.querySelector("body").setAttribute("data-theme", "light");
     localStorage.setItem("selectedTheme", "light")
